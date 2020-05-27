46ef84b71696a985006f885f27d47a8974c32247
Paulo <pauloworkjobsbr@gmail.com>
2020-05-27 20:42:00 +0100

    Add project files.

---
+++ SeaCleanSolutions/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SeaCleanSolutions.Areas.Identity.Data;
+using SeaCleanSolutions.Models;
+
+[assembly: HostingStartup(typeof(SeaCleanSolutions.Areas.Identity.IdentityHostingStartup))]
+namespace SeaCleanSolutions.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IndentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IndentityContextConnection")));
+
+                services.AddDefaultIdentity<SeaCleanSolutionsUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<IndentityContext>();
+            });
+        }
+    }
+}
