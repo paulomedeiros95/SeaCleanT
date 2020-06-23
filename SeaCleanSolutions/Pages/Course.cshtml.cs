84ed51720623b63065b10babddf4833bf9458702
Paulo <pauloworkjobsbr@gmail.com>
2020-06-23 19:41:52 +0100

    finalizado courseList

--- SeaCleanSolutions/Pages/Course.cshtml.cs
+++ SeaCleanSolutions/Pages/Course.cshtml.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SeaCleanSolutions.Pages
 {
-    public class CoursesListModel : PageModel
+    public class CourseModel : PageModel
     {
+        public void OnGet( string course)
+        {
 
+        }
     }
 }
---
+++ SeaCleanSolutions/Pages/CourseList.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SeaCleanSolutions.Areas.Application.Data;
+using SeaCleanSolutions.Areas.Identity.Data;
+using SeaCleanSolutions.Models;
+
+namespace SeaCleanSolutions.Pages
+{
+    public class CourseListModel : PageModel
+    {
+        private readonly SignInManager<SeaCleanSolutionsUser> _signInManager;
+        private readonly UserManager<SeaCleanSolutionsUser> _userManager;
+        private readonly ILogger<CourseListModel> _logger;
+
+
+        public CourseListModel(UserManager<SeaCleanSolutionsUser> userManager,
+            SignInManager<SeaCleanSolutionsUser> signInManager,
+            ILogger<CourseListModel> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public IEnumerable<Course> Courses { get; set; }
+
+
+        public async Task OnGet()
+        {
+            using (var context = new ApplicationDBContext())
+            {
+                Courses = await context.Courses.ToListAsync();
+            }
+        }
+    }
+}
